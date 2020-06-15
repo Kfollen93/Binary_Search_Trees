5f40dd7044e4f3523ae912339434dc4d60b48f73
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-06-15 13:14:08 -0700

    Added recursive function to construct tree

--- node.rb
+++ node.rb
@@ -1,13 +1,8 @@
 class Node
-    attr_accessor :left_parent, :right_parent, :left_tree, :right_tree, :left_child, :right_child, :next_node, :value
-    def initialize(value = nil, next_node = nil)
-        @left_parent = left_parent
-        @right_parent = right_parent
-        @left_tree = left_tree
-        @right_tree = right_tree
-        @left_child = left_child
-        @right_child = right_child
-        @next_node = next_node
+    attr_accessor  :left, :right, :value
+    def initialize(value = nil)
+        @left = left
+        @right = right
         @value = value
     end
 end
--- tree.rb
+++ tree.rb
@@ -5,25 +5,32 @@ class Tree
     def initialize(data_array)
         @root = nil
         @data_array = data_array
+
     end
 
-    def build_tree
-        data_sorted = data_array.uniq.sort
-        data_length = data_sorted.length
-        data_median = (data_sorted[(data_length - 1) / 2] + data_sorted[data_length / 2]) / 2.0
-        node_root = Node.new(data_median.to_i)
+def build_tree
+data_sorted = data_array.uniq.sort
+if data_sorted.empty? 
+    return nil
+else
+    return constructTreeFromArray(data_sorted, 0, data_sorted.length-1)
+end
+end
 
-        if data_sorted.empty?
-            return nil
-        elsif @root == nil
-            @root = node_root.value
-        else
-            node_root.next_node = @root
-            @root = node_root.value
-        end
+def constructTreeFromArray(data_sorted, left, right)
+    if left > right
+        return nil
+    else
+        mid = left + (right-left) / 2
+        node = Node.new(data_sorted[mid])
+        node.left = constructTreeFromArray(data_sorted, left, mid-1)
+        node.right = constructTreeFromArray(data_sorted, mid+1, right)
+        return node.value # root node
     end
+end
+
+
 end
 
 binary = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
 p binary.build_tree
-
