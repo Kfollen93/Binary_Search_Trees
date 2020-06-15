14ac36ac0b605f0f592d2d3405b7b016e5827e7a
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-06-15 10:41:07 -0700

    Renamed node to root_node

--- tree.rb
+++ tree.rb
@@ -11,19 +11,19 @@ class Tree
         data_sorted = data_array.uniq.sort
         data_length = data_sorted.length
         data_median = (data_sorted[(data_length - 1) / 2] + data_sorted[data_length / 2]) / 2.0
-        node = Node.new(data_median)
+        node_root = Node.new(data_median.to_i)
 
-        if data_array.empty?
+        if data_sorted.empty?
             return nil
         elsif @root == nil
-            @root = node.value
+            @root = node_root.value
         else
-            node.next_node = @root
-            @root = node.value
+            node_root.next_node = @root
+            @root = node_root.value
         end
     end
 end
 
 binary = Tree.new([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
-binary.build_tree
+p binary.build_tree
 
