b00cc45166d04fcad624b451e3805f288a24f6d1
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-06-16 11:55:21 -0700

    Removed mid variable

--- node.rb
+++ node.rb
@@ -1,9 +1,8 @@
 class Node
-    attr_accessor  :left, :right, :value, :mid
+    attr_accessor  :left, :right, :value
     def initialize(value = nil)
         @left = left
         @right = right
-        @mid = mid
         @value = value
     end
 end
