fccb4592b034be68e0e9ec528cafe3a9d37461f2
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-06-16 11:53:35 -0700

    Added mid variable

--- node.rb
+++ node.rb
@@ -1,8 +1,9 @@
 class Node
-    attr_accessor  :left, :right, :value
+    attr_accessor  :left, :right, :value, :mid
     def initialize(value = nil)
         @left = left
         @right = right
+        @mid = mid
         @value = value
     end
 end
