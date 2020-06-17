eb5fd4bd8bc6d634afe1503ac4c9641a7a1306b1
Kyle Follendorf <Ksfollendorf@gmail.com>
2020-06-17 12:37:22 -0700

    Formatted and replaced value with data for clarity

--- node.rb
+++ node.rb
@@ -1,9 +1,8 @@
 class Node
-
-    attr_accessor :left, :right, :data
-    def initialize(data = nil)
-        @left = left
-        @right = right
-        @data = data
-    end
+  attr_accessor :left, :right, :data
+  def initialize(data = nil)
+    @left = left
+    @right = right
+    @data = data
+  end
 end
